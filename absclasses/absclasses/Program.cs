590743146a20539097230fd5c80d79e822c7e212
Saravana K <kumarsaravana199@gmail.com>
2017-04-10 23:31:53 +0530

    Add files via upload

---
+++ absclasses/absclasses/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace absclasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fulltimeemplo fe = new fulltimeemplo()
+            {
+                id = 1234,
+                firname = "Sara",
+                lasname = "vana",
+                annualsal = 90000
+            };
+            Console.WriteLine(fe.getfullname());
+            Console.WriteLine(fe.monthlysal());
+
+            partimeemplo pe = new partimeemplo()
+            {
+                id = 1234,
+                firname = "sa",
+                lasname = "na",
+                hours = 5,
+                perhour=500
+          
+            };
+            Console.WriteLine(pe.getfullname());
+            Console.WriteLine(pe.monthlysal());
+
+        }
+
+    }
+}
