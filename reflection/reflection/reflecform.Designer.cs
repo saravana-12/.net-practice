79c989798e65d1ec8b892a88ea136b1ec32b0448
Saravana K <kumarsaravana199@gmail.com>
2017-04-10 23:43:01 +0530

    Add files via upload

---
+++ reflection/reflection/reflecform.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace reflection
+{
+    partial class reflecform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnclick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnclick
+            // 
+            this.btnclick.Location = new System.Drawing.Point(103, 76);
+            this.btnclick.Name = "btnclick";
+            this.btnclick.Size = new System.Drawing.Size(75, 23);
+            this.btnclick.TabIndex = 0;
+            this.btnclick.Text = "click";
+            this.btnclick.UseVisualStyleBackColor = true;
+            this.btnclick.Click += new System.EventHandler(this.btnclick_Click);
+            // 
+            // reflecform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnclick);
+            this.Name = "reflecform";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnclick;
+    }
+}
+
