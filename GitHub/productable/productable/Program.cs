51949fcb67f5fdfa08fa233d43123b506819030a
Saravana Kumar <kumarsaravana199@gmail.com>
2017-04-11 19:59:26 +0530

    program for product table

---
+++ GitHub/productable/productable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int row = 1; row <= 9; row++)
+            {
+                for (int col = 1; col <= 9; col++)
+                {
+                    sum = row * col;
+                    Console.Write("{0} ", sum);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- GitHub/productable/productable/Properties/AssemblyInfo.cs
+++ GitHub/productable/productable/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("absclasses")]
+[assembly: AssemblyTitle("productable")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("absclasses")]
+[assembly: AssemblyProduct("productable")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5d3d8f0e-948e-4329-b2d4-b3f6d789ae79")]
+[assembly: Guid("da767c35-41fd-4edd-8a27-855b7d680184")]
 
 // Version information for an assembly consists of the following four values:
 //
