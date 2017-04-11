778254c24ec52916a21cb3cace88ca3fa01b16d4
Saravana Kumar <kumarsaravana199@gmail.com>
2017-04-11 20:54:18 +0530

    oattern program

---
+++ GitHub/patternprint/patternprint/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patternprint
+{
+    class pattern
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            for (int j = 0; j < size; j++)
+            {
+                for (int k=0;k<j;k++)
+                    Console.Write(" ");
+                for (int i = 0; i < size - j; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+    }
+}
