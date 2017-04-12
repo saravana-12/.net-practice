99afd43a031ffca4fb9f6b9d9c71813eb856079c
Saravana Kumar <kumarsaravana199@gmail.com>
2017-04-12 14:46:59 +0530

    stringreverse

---
+++ GitHub/stringreverse/stringreverse/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("stringreverse")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("stringreverse")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("26b773b2-bc4c-4b23-bbfa-4d2f7482a616")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GitHub/stringreverse/stringreverse/strrev.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringreverse
+{
+    class strrev
+    {
+        static void Main(string[] args)
+        {
+           string outp= reverse();
+            Console.WriteLine(outp);
+        }
+    public static string reverse()
+        {
+            string revstr="";
+            Console.WriteLine("enter string");
+          string  instr = Convert.ToString(Console.Read());
+            for (int i = instr.Length - 1; i >= 1; i--)
+            {
+                revstr = revstr + instr[instr.Length - i];
+
+            }
+            Console.WriteLine("reverse string");
+            Console.WriteLine(revstr);
+            return revstr;
+        }
+    }
+}
